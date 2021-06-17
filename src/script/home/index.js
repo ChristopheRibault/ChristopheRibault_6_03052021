9e77918a10e42b3fe6775ddacb50380af03bbb52
ChristopheRibault <c.ribault.dev@gmail.com>
2021-06-17 09:42:28 +0200

    feat: enable multiple tag selection

--- src/script/home/index.js
+++ src/script/home/index.js
@@ -1,11 +1,6 @@
 import data from '../../../assets/data/data.json';
 import HomePage from './pageContent';
 
-const typeFilter = new URLSearchParams(document.location.search).get('type');
+// const typeFilter = new URLSearchParams(document.location.search).get('type');
 
-const filteredPhotographers = data.photographers
-  .filter(photographer => {
-    return typeFilter ? photographer.tags.includes(typeFilter) : true;
-  });
-
-new HomePage(filteredPhotographers);
+new HomePage(data.photographers);
